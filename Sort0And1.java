4748edabd8964e2e3b531cd48fd6fd3101bda66d
Arpit Mehta <arpitmehta06@gmail.com>
2017-12-12 21:48:13 +0530

    Sort 0 and 1 from array holding 0's and 1's in one loop.

---
+++ Sort0And1.java
@@ -0,0 +1,44 @@
+package com.apple;
+
+public class Sort0And1 {
+	void segregate0and1(int arr[], int size) 
+    {
+        /* Initialize left and right indexes */
+        int left = 0, right = size - 1;
+ 
+        while (left < right) 
+        {
+            /* Increment left index while we see 0 at left */
+            while (arr[left] == 0 && left < right)
+               left++;
+ 
+            /* Decrement right index while we see 1 at right */
+            while (arr[right] == 1 && left < right)
+                right--;
+ 
+            /* If left is smaller than right then there is a 1 at left
+               and a 0 at right.  Exchange arr[left] and arr[right]*/
+            if (left < right) 
+            {
+                arr[left] = 0;
+                arr[right] = 1;
+                left++;
+                right--;
+            }
+        }
+    }
+     
+    /* Driver Program to test above functions */
+    public static void main(String[] args) 
+    {
+    	Sort0And1 seg = new Sort0And1();
+        int arr[] = new int[]{1, 0, 0};
+        int i, arr_size = arr.length;
+ 
+        seg.segregate0and1(arr, arr_size);
+ 
+        System.out.print("Array after segregation is ");
+        for (i = 0; i < arr_size; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
